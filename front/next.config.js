4ff43c0ace1c7f46ca0be1c3bf7e40197cab6c17
linozen <linus@sehn.tech>
2021-04-24 13:02:37 +0200

    deleted unneeded stuff; added caching policy

--- front/next.config.js
+++ front/next.config.js
@@ -3,6 +3,34 @@ const withPWA = require("next-pwa");
 const prod = process.env.NODE_ENV === "production";
 
 module.exports = withPWA({
+  async headers() {
+    return [
+      {
+        // This works, and returns appropriate Response headers:
+        source: '/(.*).jpg',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+          },
+        ],
+      },
+      {
+        // This doesn't work for 'Cache-Control' key (works for others though):
+        source: '/_next/image(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            // Instead of this value:
+            value: 'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+            // Cache-Control response header is `public, max-age=60` in production
+            // and `public, max-age=0, must-revalidate` in development
+          },
+        ],
+      },
+    ]
+  },
   future: { webpack5: true },
   i18n,
   images: {
