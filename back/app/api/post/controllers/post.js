b81386bb2fb2abf7ae35424f54631a336b1d00c9
linozen <linus@sehn.tech>
2021-04-26 18:00:20 +0200

    new post controller

--- back/app/api/post/controllers/post.js
+++ back/app/api/post/controllers/post.js
@@ -5,4 +5,30 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils')
+module.exports = {
+    async find(ctx) {
+        let entities;
+        if (ctx.query._q) {
+            entities = await strapi.services.post.search(ctx.query);
+        } else {
+            entities = await strapi.services.post.find(ctx.query);
+        }
+        const url = strapi.config.get('server.url')
+        let newEntities = entities.map(e => {
+            let newEnt = {}
+            newEnt["content_en"] = e.content_en.replace(/\/uploads/, `${url}/uploads`)
+            newEnt["content_de"] = e.content_de.replace(/\/uploads/, `${url}/uploads`)
+            newEnt["title_en"] = e.title_en
+            newEnt["title_de"] = e.title_de
+            newEnt["excerpt_en"] = e.excerpt_en
+            newEnt["excerpt_de"] = e.excerpt_de
+            newEnt["slug"] = e.slug
+            newEnt["date"] = e.date
+            newEnt["coverImage"] = e.coverImage
+            newEnt["video"] = e.video === null ? { url: "no_video" } : e.video
+            return newEnt
+        })
+        return newEntities.map(entity => sanitizeEntity(entity, { model: strapi.models.post }));
+    },
+};
--- back/app/config/server.js
+++ back/app/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('WEBSITE', 'http://127.0.0.1'), // THIS ONE
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6c19e3be235d98d9c96b411e4bd63157'),
