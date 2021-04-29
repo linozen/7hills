f789ed6d7124ca27a6f5777e5c04f61fdb46ccf3
linozen <linus@sehn.tech>
2021-04-29 17:04:02 +0200

    added umami tracking code

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -8,6 +8,10 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            async defer
+            data-website-id="5610f70f-62e0-40b6-bfff-4ae5f3f97c6d"
+            src="https://umami.sevenhills-restaurant.de/umami.js"></script>
         </body>
       </Html>
     )
