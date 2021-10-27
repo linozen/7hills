ab3ede2572845318dc80b186de92042872a6db4c
linozen <linus@sehn.tech>
2021-10-27 17:29:49 +0200

    rename files to be removed

--- back/pull-data.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rsync -avhP containers:/home/deployr/7hills/data/ /home/lino/Projects/7hills/back/data/ --delete
-rsync -avhP containers:/home/deployr/7hills/uploads/ /home/lino/Projects/7hills/back/app/public/uploads/ --delete
--- back/push-data.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rsync -avhP /home/lino/Projects/7hills/back/data/ containers:/home/deployr/7hills/data/ --delete
-rsync -avhP /home/lino/Projects/7hills/back/app/public/uploads/ containers:/home/deployr/7hills/uploads --delete
