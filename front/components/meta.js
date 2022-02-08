01280e9b65d74a5dcafde86e56399ba4e4b5f0b8
linozen <linus@sehn.tech>
2022-02-08 18:13:13 +0100

    remove unused

--- front/components/meta.js
+++ front/components/meta.js
@@ -31,12 +31,6 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#001300" />
       <meta property="og:image" content="/large_forest.jpg" />
-      <script
-        async
-        defer
-        data-website-id="c53a4784-1f3e-42de-975a-4cb3e64655c4"
-        src="https://umami.sevenhills-restaurant.de/umami.js"
-      ></script>
     </Head>
   );
 }
